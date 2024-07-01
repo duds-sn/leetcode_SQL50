edd872516c2b244efad13f71cbcf545fb5170db6
Eduardo Schneider Nering <eduardonering@hotmail.com>
2024-06-30 23:42:24 -0300

    added solved #23 problem

--- Basic Aggregate Functions/#23 - Number of Unique Subjects Taught by Each Teacher/solution.sql
+++ Basic Aggregate Functions/#23 - Number of Unique Subjects Taught by Each Teacher/solution.sql
@@ -1,34 +1,16 @@
 /*
-Write a solution to report the fraction of players that logged in again on the day after the day they first logged in, rounded to 2 decimal places. In other words, you need to count the number of players that logged in for at least two consecutive days starting from their first login date, then divide that number by the total number of players.
-The result format is in the following example.
-+-----------+
-| fraction  |
-+-----------+
-| 0.33      |
-+-----------+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+Return the result table in any order.
+The result format is shown in the following example.
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
 */
-WITH first_login AS (
-    SELECT
-        player_id,
-        MIN( event_date ) AS first_login_date
-    FROM Activity
-    GROUP BY player_id
-)
-
 SELECT
-    ROUND(    
-        COUNT(DISTINCT a.player_id) FILTER (
-                WHERE ac.player_id IS NOT NULL
-                    AND fl.first_login_date = a.event_date
-            )
-            / COUNT(DISTINCT a.player_id)::numeric,
-        2
-    ) AS fraction
-FROM Activity a
-LEFT JOIN 
-    Activity ac 
-        ON a.player_id = ac.player_id
-            AND a.event_date + 1 = ac.event_date
-LEFT JOIN
-    first_login fl
-        ON a.player_id = fl.player_id
+    teacher_id,
+    COUNT(DISTINCT subject_id) cnt
+FROM Teacher
+GROUP BY 1
