e09ec5838efc89af88a951c7c44e22ca51387227
Eduardo Schneider Nering <eduardonering@hotmail.com>
2024-07-25 23:26:36 -0300

    added solved problem #45

---
+++ Advanced String Functions, Regex, Clause/#45 - Patients With a Condition/solution.sql
@@ -0,0 +1,17 @@
+/*
+Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+
+Return the result table in any order.
+
+The result format is in the following example.
++------------+--------------+--------------+
+| patient_id | patient_name | conditions   |
++------------+--------------+--------------+
+| 3          | Bob          | DIAB100 MYOP |
+| 4          | George       | ACNE DIAB100 | 
++------------+--------------+--------------+
+*/
+SELECT
+	*
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
