32f1c7a9c61317313d159d9357472915b95e31b4
Eduardo Schneider Nering <eduardonering@hotmail.com>
2024-07-29 11:50:04 -0300

    added solved problem #50

---
+++ Advanced String Functions, Regex, Clause/#50 - Find Users With Valid E-Mails/solution.sql
@@ -0,0 +1,22 @@
+/*
+Write a solution to find the users who have valid emails.
+
+A valid e-mail has a prefix name and a domain where:
+
+The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+The domain is '@leetcode.com'.
+Return the result table in any order.
+
+The result format is in the following example.
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
++---------+-----------+-------------------------+
+*/
+SELECT
+	*
+FROM Users
+WHERE mail ~ '^[A-Za-z][-A-Za-z0-9_\.]*@leetcode\.com$'
